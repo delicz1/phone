53ea106d0c84e4ac08aa0dc725c57437bb4031f2
kozak <kozak.honza@gmail.com>
2014-04-12 13:25:56 +0200

    WP8

---
+++ PhoneMobileTracker/AppSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneMobileTracker {
+    class AppSetting {
+        public const string PREF_USER_NAME = "pref_user_name";
+        public const string PREF_USER_PASSWORD = "pref_user_password";
+        public const string PREF_IMEI = "pref_imei";
+    }
+}
